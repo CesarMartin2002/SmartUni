51cc05dbd2b8e4b4af6e1fd0fccce161075002fa
Valeriia632 <valeria.villamares@edu.uah.es>
2023-05-03 12:09:57 +0200

    movido el signup

--- CODIGO/BACK/endpoints/sesion.py
+++ CODIGO/BACK/endpoints/sesion.py
@@ -14,3 +14,15 @@ async def login(request: Request):
     contrasena = data["password"]
     usuario = logica.login(correo, contrasena)
     return logica.respuesta_exitosa(usuario)
+
+@router.post("/signup")
+async def registro(request: Request):
+    """
+    Este endpoint permite al usuario registrarse
+    """
+    data = await request.json()
+    print("data: ", data)
+    correo = data["correo"]
+    contrasena = data["password"]
+    usuario = logica.registro(correo, contrasena)
+    return logica.respuesta_exitosa(usuario)
