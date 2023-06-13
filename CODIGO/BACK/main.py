d14013a6c48b1da091c1b582efafe3531013414c
CesarMartin2002 <cesar.marting@edu.uah.es>
2023-06-13 03:47:58 +0200

    cambios en memoria y en la documentacion

--- CODIGO/BACK/main.py
+++ CODIGO/BACK/main.py
@@ -5,12 +5,22 @@ from starlette.exceptions import HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import PlainTextResponse
+from fastapi.openapi.docs import get_swagger_ui_html
 
 #importamos todos los endpoints
 from endpoints import taquillas, paginasHTML, aulas, sesion, productos, pedidos, nfc
 import logica
 
-app = FastAPI(title="SmartUni", description="Documentación de la API de SmartUni", version="1.0.0", redoc_url="/api/redoc", contact={"name": "Ver repositorio", "url": "https://github.com/CesarMartin2002/Proyecto-ubicua"},)
+app = FastAPI(title="SmartUni", description="Documentación de la API de SmartUni", version="1.0.0",docs_url=None, contact={"name": "Ver repositorio", "url": "https://github.com/CesarMartin2002/Proyecto-ubicua"},)
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title,
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui-bundle.js",
+        swagger_css_url="/static/custom.css")
 
 #region manejo de errores
 # Middleware to handle all exceptions
